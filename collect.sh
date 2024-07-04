004e7e9016932832946f07e1d2bb4823312f023e
Ryan Gannon <ryanmgannon@gmail.com>
2024-07-04 00:40:59 -0400

    no dots

--- collect.sh
+++ collect.sh
@@ -8,6 +8,7 @@ mkdir -p $SCRIPT_DIR/doom
 mkdir -p $SCRIPT_DIR/hypr
 
 cp ~/.zshrc $SCRIPT_DIR/zshrc
+cp ~/.sbclrc $SCRIPT_DIR/sbclrc
 cp ~/.config/nvim/init.vim $SCRIPT_DIR/vim/
 cp ~/.config/nvim/lua/config.lua $SCRIPT_DIR/vim/lua/
 cp ~/.config/nvim/autoload/* $SCRIPT_DIR/vim/autoload/
